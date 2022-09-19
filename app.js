cf29406447a901b5ab1044c72ccdaaad733eed4e
Binij Shrestha <shresthabinij@gmail.com>
2022-09-19 18:51:14 +0545

    second commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+let app = express();
+
+app.listen(3000, () => {
+
+    console.log("Server running !!");
+
+})
--- src/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-var app = express();
-
-app.listen(procces.env.PORT || 3000, () => {
-
-    console.log("Server running !!");
-
-});
