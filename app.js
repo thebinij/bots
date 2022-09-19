c5432035d798db8a15e548595af78ac689c1df91
Binij Shrestha <shresthabinij@gmail.com>
2022-09-19 19:00:35 +0545

    get api

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 let app = express();
 
+app.get("/", function (request, response) {
+    response.send("Hi Human!")
+})
+
 app.listen(3000, () => {
-
     console.log("Server running !!");
-
 })
