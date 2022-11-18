c183e44ac57720e363b75a392885ddf67395b6eb
Binij Shrestha <shresthabinij@gmail.com>
2022-11-18 20:30:12 +0545

    feat: api routes, waitress to serve

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+from waitress import serve
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,9 +15,23 @@ def home():
         return render_template("index.html", output=on_message(text), user_text=text)
 
 
+@app.route('/api', methods=['GET', 'POST'])
+def api():
+    if request.method == "GET":
+        return jsonify({'data': 'It is now working'})
+    if request.method == "POST":
+        username = request.json['name']
+        return jsonify({"reponse": f"Hello {username}"})
+
+
 def on_message(message):
     return (f'Hello There! {message}')
 
 
+mode = 'dev'
+
 if __name__ == "__main__":
-    app.run()
+    if mode == 'dev':
+        app.run(host="0.0.0.0", port=5201, debug=True)
+    else:
+        serve(app, host="0.0.0.0", port=8080)
